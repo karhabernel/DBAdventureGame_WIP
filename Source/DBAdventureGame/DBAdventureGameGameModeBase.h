24d3b06f8d370ffd612acfffe975e6bd2f3d2745
Karha <124255059+karhabernel@users.noreply.github.com>
2024-10-09 11:32:45 +0900

    1009_UISetup

---
+++ Source/DBAdventureGame/DBAdventureGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DBAdventureGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DBADVENTUREGAME_API ADBAdventureGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
